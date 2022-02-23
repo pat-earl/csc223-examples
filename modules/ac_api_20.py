d6e94c57d365bce241062f9034e846c5dbb4b1ad
Earl <earl@kutztown.edu>
2022-02-23 12:46:15 -0500

    section 20 api request

---
+++ modules/ac_api_20.py
@@ -0,0 +1,27 @@
+from urllib.request import urlopen
+import json
+import csv
+import pprint
+
+api_url = 'https://acnhapi.com/v1/'
+
+api_request = urlopen(api_url + 'bugs')
+
+api_request = api_request.read()
+
+data = json.loads(api_request)
+
+with open('bug_data.csv', 'w') as f:
+    writer = csv.writer(f)
+
+    headers = ['Name', 'ID', 'Rarity']
+
+    writer.writerow(headers)
+
+    for bug in data:
+        b = [
+            data[bug]['name']['name-USen'], 
+            data[bug]['id'], 
+            data[bug]['availability']['rarity']
+        ]
+        writer.writerow(b)
