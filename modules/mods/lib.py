3e825a83119f444d65b33a1eb2d2e6be940d6e34
Earl <earl@kutztown.edu>
2022-03-02 12:41:14 -0500

    03/02 stuff

---
+++ modules/mods/lib.py
@@ -0,0 +1,8 @@
+def another_function(name):
+    print("Hello from another_function,", name)
+
+def test(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    print(test(1, 2))
---
+++ modules/mods/run.py
@@ -0,0 +1,11 @@
+import lib
+
+def hello():
+    print("Hello World!")
+
+def main():
+    hello()
+    lib.another_function("Bill")
+
+if __name__ == "__main__":
+    main()
